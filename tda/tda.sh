9897a0d4533a61fda13a03fca3f6411085c07f72
Luqipin <luqipin@live.com>
2023-04-16 21:29:41 +0800

    Update 20230416

--- tda/tda.sh
+++ tda/tda.sh
@@ -46,8 +46,9 @@ EOF
 	if [ -f /root/data/${data_date}/${table}.txt ];then
 		rm /root/data/${data_date}/${table}.txt
 	fi
-	hadoop fs -get /var/hive/warehouse/data_output/${data_date}/${table}/000000_0 /root/data/${data_date}/${table}.txt
-	hadoop fs -rm /var/hive/warehouse/data_output/${data_date}/${table}/000000_0
+	# hadoop fs -get /var/hive/warehouse/data_output/${data_date}/${table}/000000_0 /root/data/${data_date}/${table}.txt
+	hadoop fs -getmerge /var/hive/warehouse/data_output/${data_date}/${table} /root/data/${data_date}/${table}.txt
+	hadoop fs -rm -r /var/hive/warehouse/data_output/${data_date}/${table}
 		else
 			echo "未找到${table}配置，请检查tda.cfg！"
 		fi
@@ -55,8 +56,47 @@ EOF
 
 elif [ ${db_type} == 'mysql' ]; then
 	echo "导出数据库${db_type}"
-	v_sql="mysql -u username -p password database_name -e "SELECT * FROM table_name INTO OUTFILE 'data_file_name.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'""
-	echo ${v_sql}
+		while read line; do
+		config_name=$(echo "$line" | cut -d = -f 1|  rev |cut -c 2- | rev )
+		TERMINATED=$(echo "$line" | cut -d = -f 1 | rev |cut -c -1 )
+		exp_sql=$(echo "$line" | cut -d = -f 2 )
+		
+		sql_1=$(echo "$exp_sql" | awk -F "from" '{print $1}' | sed 's/[[:space:]]*$//')
+		sql_2=$(echo "$exp_sql" | awk -F "from" '{print $2}')
+
+		if [ "$config_name" == ${table} ]; then
+			echo "正在导出${table}"
+
+			read -r -d '' v_exp << 'EOF'
+            ${sql_1}
+            INTO OUTFILE '/root/data/${data_date}/${table_name}.txt'
+            FIELDS TERMINATED BY '${delimiter}'
+            from ${sql_2}
+
+EOF
+
+
+			
+			
+			v_exp=${v_exp//\$\{sql_1\}/${sql_1}}
+			v_exp=${v_exp//\$\{sql_2\}/${sql_2}}
+			v_exp=${v_exp//\$\{delimiter\}/${TERMINATED}}
+			v_exp=${v_exp//\$\{data_date\}/${data_date}}
+			v_exp=${v_exp//\$\{table_name\}/${table}}
+			echo "导出语句
+			${v_exp}
+			"
+
+	if [ -f /root/data/${data_date}/${table}.txt ];then
+		rm /root/data/${data_date}/${table}.txt
+	fi
+
+		else
+			echo "未找到${table}配置，请检查tda.cfg！"
+		fi
+	done < tda.cfg
+	
+	
 elif [ ${db_type} == 'oracle' ]; then
 	echo "导出数据库${db_type}"
 else
